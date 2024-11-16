f8cb3db2e8999925ac97a03ab4b509d1462df34c
Fyodor <voropaevfedor2005@mail.ru>
2024-11-16 19:21:36 +0300

    feat: finish the user-service refactor and add new functionality, change exception handling

--- user-service/src/main/java/ru/itis/marketplace/userservice/controller/payload/cart/NewCartItemPayload.java
+++ user-service/src/main/java/ru/itis/marketplace/userservice/controller/payload/cart/NewCartItemPayload.java
@@ -1,6 +1,5 @@
 package ru.itis.marketplace.userservice.controller.payload.cart;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
