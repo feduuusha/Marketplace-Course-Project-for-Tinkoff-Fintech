41b3a44b92c2318fd2c015ab2a355bc6a8034c1b
Fyodor <voropaevfedor2005@mail.ru>
2024-12-13 00:48:35 +0300

    fix: fix small bug in StripeWebHookRestController

--- user-service/src/main/java/ru/itis/marketplace/userservice/controller/StripeWebHookRestController.java
+++ user-service/src/main/java/ru/itis/marketplace/userservice/controller/StripeWebHookRestController.java
@@ -11,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.itis.marketplace.userservice.service.WebHookService;
 
-import java.util.Optional;
-
 @Tag(name = "Stripe Web Hook Rest Controller", description = "Controller for handle Stripe webhooks")
 @RestController
 @RequestMapping("api/v1/webhooks/orders")
